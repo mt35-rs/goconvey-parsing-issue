e623f60a7a58171f920ce650ba77c01cd466170f
Michael Tiller <michael.tiller@ricardo.com>
2021-10-20 15:49:59 -0400

    Demonstrate a working system

--- add_test.go
+++ add_test.go
@@ -4,16 +4,11 @@ import (
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestAdd(t *testing.T) {
 	Convey("First stage", t, func() {
 		So(5, ShouldEqual, 5)
-	})
-
-	Convey("Second stage", t, func() {
-		assert.Equal(t, 5, 5)
-		assert.Equal(t, 5, 6)
+		So(5, ShouldEqual, 6)
 	})
 }
